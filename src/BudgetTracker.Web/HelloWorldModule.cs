fdc534948b5eaa1ee9b1dbc04641df4a928740d6
Aaron Shiels <aaron.shiels@outlook.com>
2014-12-26 17:54:24 +1000

    Hello world module

---
+++ src/BudgetTracker.Web/HelloWorldModule.cs
@@ -0,0 +1,17 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetTracker.Web
+{
+    public class HelloWorldModule : NancyModule
+    {
+        public HelloWorldModule()
+        {
+            Get["/"] = _ => "Hello world";
+        }
+    }
+}
